9c829f4c439f7db4e70fb5e4e49d3c418557c105
Ruth Ramya <ramya.b228@gmail.com>
2023-02-13 16:03:27 +0530

    project setup completed

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeySlggoPfs0XdqIJAZQ6KMkqDzKBYCrE",
+  authDomain: "recipe-book-89a8c.firebaseapp.com",
+  projectId: "recipe-book-89a8c",
+  storageBucket: "recipe-book-89a8c.appspot.com",
+  messagingSenderId: "425156932566",
+  appId: "1:425156932566:web:112f04bc52f3f61d76aa77",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
